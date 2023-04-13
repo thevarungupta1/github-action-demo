df6eed409d5ff2f12b4a3fdbb8e15a33ba2ee3bc
Varun Gupta <thevarungupta5@gmail.com>
2023-04-13 10:17:56 +0530

    initial commit

---
+++ src/test/java/com/thevarungupta/spring/sample/app/SpringSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thevarungupta.spring.sample.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
